3f1015fc2ad50259e7681e8820f5762945003211
Tatenda Musiiwa <136474718+Dr-OCCO@users.noreply.github.com>
2024-09-04 03:12:45 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1200, // Duration of animations in milliseconds
+        once: true // Whether animations should happen only once
+    });
+});
